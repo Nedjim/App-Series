c30995a49ba3fbed2ee758099fdad6cc56561b57
Nedjim <nedjim.danimon@gmail.com>
2017-12-23 00:20:45 +0100

    ADD linter

--- server/config.js
+++ server/config.js
@@ -1,6 +1,4 @@
-const dotenv = require('dotenv').config();
-
 module.exports = {
-    'port': process.env.PORT || 8080,
-    'api_key': process.env.API_KEY
+  port: process.env.PORT || 8080,
+  api_key: process.env.API_KEY,
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const config = require('./config.js');
+const express = require('express')
+const config = require('./config.js')
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>APP Series</h1>')
-});
+  res.send('<h1>APP Series</h1>')
+})
 
 app.listen(config.port, () => {
-    console.log(`The application is running on localhost:${config.port}`)
-});
+  /* eslint-disable no-console */
+  console.log(`The application is running on localhost:${config.port}`)
+})
